ce2c0aba2802580fef194eed1b3d1a5ca261dff5
pauloalfeu <127105307+pauloalfeu@users.noreply.github.com>
2024-12-02 14:37:50 -0300

    update

--- ifmenu/menuapp/models.py
+++ ifmenu/menuapp/models.py
@@ -8,12 +8,41 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.email
 
+CATEGORY_CHOICES = [
+    ('salgados', 'Salgados'),
+    ('doces', 'Doces'),
+    ('bebidas', 'Bebidas'),
+    ('outros', 'Outros'),
+    ('em promoção', 'Em Promoção'),
+]
+
 class Produto(models.Model):
     nome_prod = models.CharField(max_length=200)
-    preco_prod = models.FloatField()
+    valor_und = models.FloatField()
     descricao_prod = models.CharField(max_length=600)
+    category_prod = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
     img_prod = models.ImageField(upload_to='produtos_imagens/')
+    qtd_disp = models.IntegerField()
     def __str__(self):
         return self.nome_prod
-    
 
+# Para obter todos os produtos da categoria "lanche"
+#lanches = Produto.objects.filter(category_prod='lanche')
+    
+STATUS_CHOICES = [
+    ('pendente', 'Pendente'),
+    ('entregue', 'Entregue'),
+    ('cancelado', 'Cancelado'),
+    ('finalizado', 'Finalizado'),
+]
+
+class Pedido(models.Model):
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pendente')
+    valor_total = models.DecimalField(max_digits=10, decimal_places=2)
+    data_pedido = models.DateTimeField(default=timezone.now)
+    cliente = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    itens_pedido = models.ForeignKey(ItensPedido, on_delete=models.CASCADE)
+
+class ItensPedido(models.Model):
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    quantidade = models.PositiveSmallIntegerField(null=True, blank=True)
