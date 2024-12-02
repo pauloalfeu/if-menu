f99037252e132fca9692c9622911f33c5edc05ec
pauloalfeu <127105307+pauloalfeu@users.noreply.github.com>
2024-12-02 11:57:20 -0300

    update

--- ifmenu/menuapp/models.py
+++ ifmenu/menuapp/models.py
@@ -1,22 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-class Cliente(AbstractUser):
-    contato = models.CharField(max_length=200, null=True, blank=True)
+class CustomUser(AbstractUser):
+    contato = models.CharField(max_length=100, null=True, blank=True)
     email = models.EmailField(unique=True, null=False)
-
-
-class Gerente(models.Model):
-     login = models.CharField(max_length=200)
-     senha = models.CharField(max_length=200)
-     def __str__(self):
-         return self.login
+    is_manager = models.BooleanField(default=False, null=False)
+    def __str__(self):
+        return self.email
 
 class Produto(models.Model):
     nome_prod = models.CharField(max_length=200)
     preco_prod = models.FloatField()
     descricao_prod = models.CharField(max_length=600)
-    img_prod = models.CharField(max_length=900)
+    img_prod = models.ImageField(upload_to='produtos_imagens/')
     def __str__(self):
         return self.nome_prod
     
